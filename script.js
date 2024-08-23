9febae59b0c1ac60c99e7b07c9918218c128d3a7
EQ767 <zyqcmbs@gmail.com>
2024-08-23 08:35:35 +0800

    Update script.js

--- script.js
+++ script.js
@@ -18,6 +18,7 @@ function loadFileContent(filename) {
         .then(htmlContent => {
             document.getElementById('file-content').innerHTML = htmlContent;
             document.getElementById('modal').style.display = 'block';
+            document.getElementById('modal-overlay').style.display = 'block'; // 显示遮罩层
         })
         .catch(error => {
             alert('Error loading file content');
@@ -27,5 +28,6 @@ function loadFileContent(filename) {
 
 function closeModal() {
     document.getElementById('modal').style.display = 'none';
+    document.getElementById('modal-overlay').style.display = 'none'; // 隐藏遮罩层
     document.getElementById('file-content').innerHTML = '';
 }
